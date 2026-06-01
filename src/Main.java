b1ae83e60896e36d852a19225a649d9719b36c10
Juan Pablo Meza Olivera <jmezao@unicartagena.edu.co>
2026-05-31 22:24:51 -0500

    cambio en main java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String []args){
         System.out.println("Inicio del proyecto by Juan Pablo Meza Olivera");
+        System.out.printf("prueba cambio git hub");
     }
 
 
